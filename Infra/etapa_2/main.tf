a93e55703b5ed4ec33e904c06cb961a6498bbe36
Ccarlito <carl.munozu@duocuc.cl>
2026-05-22 04:55:22 -0400

    develop: Se ajusta el rol de aws como el predeterminado de LAB.

--- Infra/etapa_2/main.tf
+++ Infra/etapa_2/main.tf
@@ -12,28 +12,15 @@ provider "aws" {
 }
 
 # ==========================================
-# IAM ROLE PARA QUE EC2 PUEDA ACCEDER A ECR
+# IAM - USA EL ROL PREEXISTENTE DE AWS ACADEMY
 # ==========================================
-resource "aws_iam_role" "ec2_role" {
-  name = "${var.project_name}-ec2-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
-      Principal = { Service = "ec2.amazonaws.com" }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecr_policy" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+data "aws_iam_role" "lab_role" {
+  name = "LabRole"
 }
 
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "${var.project_name}-ec2-profile"
-  role = aws_iam_role.ec2_role.name
+  role = data.aws_iam_role.lab_role.name
 }
 
 
